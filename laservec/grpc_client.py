19048410a25a7d221b404bc0e9cfd369b221eb62
cris-almodovar <cris-almodovar@users.noreply.github.com>
2019-12-26 18:32:32 +0800

    Vagrant changes

---
+++ laservec/grpc_client.py
@@ -0,0 +1,46 @@
+# Copyright 2019 Cris Almodovar
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import grpc
+from laservec.laservec_pb2_grpc import LaserGrpcApiStub
+from laservec.laservec_pb2 import VectorizeRequest
+
+
+class LASER(object):
+    def __init__(self, url: str):
+        self.url = url
+        self.channel = grpc.insecure_channel(url)
+        self.stub = LaserGrpcApiStub(self.channel)
+
+    def close(self):
+        self.stub = None
+        self.channel.close()
+        self.channel = None
+
+    def vectorize(self, lang: str, text: str):
+        assert self.channel is not None
+        assert self.stub is not None
+
+        req = VectorizeRequest(lang=lang, text=text)
+        res = self.stub.vectorize(req)
+        if res:
+            return res.lang, list(res.embedding)
+        return None, None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
---
+++ start_laservec_server.sh
@@ -0,0 +1,7 @@
+docker run \
+  --name laservec \
+  --detach \
+  --rm \
+  --publish 8100:8100 \
+  calmodovar/laservec
+
